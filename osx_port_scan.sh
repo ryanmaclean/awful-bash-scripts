0836845476c1033441328be8d73c684ada528d66
Ryan MacLean <ryan@ryanmaclean.com>
2016-04-24 22:57:37 -0700

    Create osx_port_scan.sh

---
+++ osx_port_scan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Turn on job control to do more than one at a time
+set -m
+
+# Check to see if Homebrew is installed, and install it if it is not
+command -v brew >/dev/null 2>&1 || { echo >&2 "You will need Homebrew to use this tool, installing now"; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+
+# Check to see if `netcat` is installed, install it if it is not
+command -v netcat >/dev/null 2>&1 || { echo >&2 "You will also need netcat in order to use this tool, installing it now"; brew install netcat; }
+
+# Prompt the user for the port which we will scan
+read -p "Which port would you like to scan? The default is [80]:" PORT
+PORT=${PORT:-80}
+
+# Prompt the user for the C-class range we will scan
+read -p "For which range? The default is [10.0.1.]:" RANGE
+RANGE=${RANGE:-10.0.1.}
+
+# Removed the start and end IP blocks and scan 255 by default
+#echo "From what starting IP? The default is 1"
+#read -p "From what starting IP? The default is [1]:" STARTIP
+#STARTIP=${STARTIP:-1}
+
+for i in {1..254}; do netcat -vnz -w 1 $RANGE$i $PORT; done
+
+# Turn off job control once more
+set +m
