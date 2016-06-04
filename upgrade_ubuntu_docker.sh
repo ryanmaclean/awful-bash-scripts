ca04d3df156d22339654ed1a218c1cf6d538657c
Ryan MacLean <ryan@ryanmaclean.com>
2016-06-03 20:12:54 -0700

    Update upgrade_ubuntu_docker.sh

--- upgrade_ubuntu_docker.sh
+++ upgrade_ubuntu_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Don't run this as root as you'll not add your user to the docker group
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -9,5 +11,5 @@ sudo apt-get install -y linux-image-extra-$(uname -r)
 sudo rm -rf /etc/default/docker
 sudo apt-get install -y docker-engine
 sudo service docker start
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker `echo $USER`
 docker -v
