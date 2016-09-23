9530c97d9a94a7d5f95154226a132fb9fe6e8172
Ryan MacLean <ryan@ryanmaclean.com>
2016-09-23 13:44:55 -0700

    Adding new awful script for Terraform and Packer
    this script will grab the most recent versions of both Hashicorp Terraform and Packer, download them, unzip and add them to your path.

---
+++ install_recent_terraform_packer.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Get URLs for most recent versions
+terraform_url=$(curl --silent https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "linux.*64" | head -1 | awk -F[\"] '{print $4}')
+packer_url=$(curl --silent https://releases.hashicorp.com/index.json | jq '{packer}' | egrep "linux.*64" | head -1 | awk -F[\"] '{print $4}')
+
+# Create a move into directory.
+cd
+mkdir packer
+mkdir terraform && cd $_
+
+# Download Terraform. URI: https://www.terraform.io/downloads.html
+curl -o terraform.zip $terraform_url
+# Unzip and install
+unzip terraform.zip
+
+# Change directory to Packer
+cd ~/packer
+
+# Download Packer. URI: https://www.packer.io/downloads.html
+curl -o packer.zip $packer_url
+# Unzip and install
+unzip packer.zip
+
+echo '
+# Terraform & Packer Paths.
+export PATH=~/terraform/:~/packer/:$PATH
+' >>~/.bash_profile
+
+source ~/.bash_profile
