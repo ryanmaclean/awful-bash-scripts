afe7b5ad426775de553046a6aca68a58e97a5b9b
Ryan MacLean <ryan@ryanmaclean.com>
2016-10-25 16:39:21 -0700

    Update old.sh

--- old.sh
+++ old.sh
@@ -1,4 +1,8 @@
-
 #!/usr/bin/env bash
 
+# This command only takes one argument, a filename, 
+# and renames the file to the same name as it had previsouly, with .old appended to the end. 
+
 mv $1{,.old}
+
+echo $1 renamed to $1.old
