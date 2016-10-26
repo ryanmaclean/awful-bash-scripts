63508819bde18ece609415f332975060d7296f82
Ryan MacLean <ryan@ryanmaclean.com>
2016-10-26 13:11:03 -0700

    Update get_vagrant_version.sh

--- get_vagrant_version.sh
+++ get_vagrant_version.sh
@@ -1,1 +1,5 @@
+#!/usr/bin/env bash
+
+# Get the latest version number for Hashicorp Terraform
+
 curl -v --silent https://releases.hashicorp.com/terraform/ 2>&1 | grep -i -e "<a href" | head -2 | grep terraform | awk -F[\>\<] '{print $3}'
