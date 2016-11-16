919b22a63c9e38a1dd1684581ad3855cd0a5b76c
Ryan MacLean <ryan@ryanmaclean.com>
2016-11-16 12:14:41 -0800

    Update erl_dhcp_leases.sh

--- erl_dhcp_leases.sh
+++ erl_dhcp_leases.sh
@@ -2,4 +2,8 @@
 
 # Get current DHCP leases and hostnames registered on your Ubiquity EdgeRouter Lite 3
 
-show dhcp leases | awk '{print $1, $NF}' | tail -n +3
+## First we set run to our vyatta cmd wrapper so we can call multiple commands
+run=/opt/vyatta/bin/vyatta-op-cmd-wrapper
+
+## Next we call our wrapper, then our command
+$run show dhcp leases | tail -n +3 | awk '{print $1, $NF}'
