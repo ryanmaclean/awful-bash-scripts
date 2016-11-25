8f2ccadbfc125974dc36fba668a8d59c09fdb529
Ryan MacLean <ryan@ryanmaclean.com>
2016-11-25 14:19:46 -0800

    Update gopath.sh

--- gopath.sh
+++ gopath.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Make sure your GOPATH is set correctly on OSX
-export GOPATH=$(which go) && export PATH=$GOPATH/bin:$PATH
+mkdir ~/go
+export GOPATH=$HOME/go && export PATH=$GOPATH/bin:$PATH
