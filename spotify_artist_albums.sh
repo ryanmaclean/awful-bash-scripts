ed3af18f9201565fe4bb360cef97add6a442c235
Ryan MacLean <ryan@ryanmaclean.com>
2016-12-20 16:36:44 -0800

    Create spotify_artist_albums.sh

---
+++ spotify_artist_albums.sh
@@ -0,0 +1,3 @@
+ARTIST = spoon
+ARTIST_ID = curl --silent -X GET "https://api.spotify.com/v1/search?q=$ARTIST&type=artist&limit=1" | jq '.artists.items | .[length-1].id'
+ARTIST_ALBUMS = curl --silent -X GET "https://api.spotify.com/v1/artists/$ARTIST_ID/albums"
