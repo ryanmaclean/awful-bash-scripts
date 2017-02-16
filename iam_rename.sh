f8397500acdc4297687f57fd4d31fa8efea6cf98
Ryan MacLean <ryan@ryanmaclean.com>
2017-02-16 13:54:05 -0800

    Update iam_rename.sh

--- iam_rename.sh
+++ iam_rename.sh
@@ -4,8 +4,11 @@
 
 ## Takes three arguments, in order, like `iam_rename.sh jill.thornton jill.smith test-env`
 
+## Defining vars so one-liner is easier to read
 OLDIAM=$1
 NEWIAM=$2
 PROFILE=$3
 
+## The one-liner itself, if you're just changing one user, 
+## updating and pasting this line may be the fastest approach.
 aws iam update-user --user-name $OLDIAM --new-user-name $NEWIAM --profile $PROFILE
