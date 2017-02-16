60c19b6346182b29170729d182fa7c297980f56f
Ryan MacLean <ryan@ryanmaclean.com>
2017-02-16 13:56:03 -0800

    Update iam_rename.sh

--- iam_rename.sh
+++ iam_rename.sh
@@ -3,6 +3,11 @@
 ## Rename IAM user(s) in AWS for a specific profile
 
 ## Takes three arguments, in order, like `iam_rename.sh jill.thornton jill.smith test-env`
+## Check that we have the right amount of arguments (3)
+if test "$#" -ne 3; then
+    echo "Missing either the old IAM name, new IAM name or AWS profile"
+    echo "example - iam_rename.sh jill.thornton jill.smith test-env"
+fi
 
 ## Defining vars so one-liner is easier to read
 OLDIAM=$1
