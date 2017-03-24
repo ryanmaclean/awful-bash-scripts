4c804e5dd1f95ca77c2121c71d26f7856364b5e9
Matthieu Dolci <matthieu.dolci@gmail.com>
2017-03-24 13:34:15 -0700

    Updating few little things

--- install_recent_terraform_packer.sh
+++ install_recent_terraform_packer.sh
@@ -2,19 +2,29 @@
 
 # Modified version of this gist: https://gist.github.com/Adron/90863e51c8c5c0ad2049890bcd8abbfb 
 
+# Install requirements
+sudo apt-get update && apt-get install jq unzip
+
 # Get URLs for most recent versions
 terraform_url=$(curl --silent https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "linux.*64" | sort -rh | head -1 | awk -F[\"] '{print $4}')
 packer_url=$(curl --silent https://releases.hashicorp.com/index.json | jq '{packer}' | egrep "linux.*64" | sort -rh | head -1 | awk -F[\"] '{print $4}')
 
-# Create a move into directory.
+# Create directories
 cd
 mkdir packer
-mkdir terraform && cd $_
+mkdir terraform
+
+
+# Change directory to terraform
+cd ~/terraform
 
 # Download Terraform. URI: https://www.terraform.io/downloads.html
 curl -o terraform.zip $terraform_url
 # Unzip and install
 unzip terraform.zip
+# Remove zip
+rm terraform.zip
+
 
 # Change directory to Packer
 cd ~/packer
@@ -23,6 +33,9 @@ cd ~/packer
 curl -o packer.zip $packer_url
 # Unzip and install
 unzip packer.zip
+# Remove zip
+rm packer.zip
+
 
 echo '
 # Terraform & Packer Paths.
