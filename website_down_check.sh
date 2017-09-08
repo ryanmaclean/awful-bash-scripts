0cb22094a0880412d7f6f68358a8d236b33feb6c
Ryan MacLean <ryan@ryanmaclean.com>
2017-09-08 13:18:35 -0700

    Adding Comments
    Also updating the loop to make it more readable

--- website_down_check.sh
+++ website_down_check.sh
@@ -1,5 +1,19 @@
 #!/bin/env bash 
 
 # Check a few websites using a list to see if they are down
- 
-for i in qa dev test prod; do curl -fsS https://service-$i.example.com -o /dev/null || echo $i "website is down"; done
+
+## The sites are in the format:
+## https://service-qa.example.com
+## https://service-dev.example.com
+## https://service-test.example.com
+## https://service-prod.example.com
+
+## The loop will silently `curl` the URLs, pipe output to /dev/null,
+## and if it fails, echo that the site is down, prefixed with the site name.
+## Any action can be done instead or in addition to the echo, for example
+## you could make a rest call to an alert mechanism with the failure message
+## as a payload. 
+
+for i in qa dev test prod; do
+    curl -fsS https://service-$i.example.com -o /dev/null || echo $i "website is down";
+done
