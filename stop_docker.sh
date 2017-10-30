1c5165d81d8e4cc170ba621cc3e206f155f6fbda
Ryan MacLean <ryan@ryanmaclean.com>
2017-10-30 16:36:48 -0700

    Update stop_docker.sh

--- stop_docker.sh
+++ stop_docker.sh
@@ -1,4 +1,7 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+
+# Enumerate all running containers, run `docker kill` on each of the IDs
+# Let the user know via console what is happening. 
 
 for i in $(docker ps | awk '{print $1}' | tail -n +2); 
   do echo Killing Docker container: && docker kill $i && echo done;
