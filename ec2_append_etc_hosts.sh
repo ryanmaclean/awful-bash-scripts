58f2742e9747f25e68c788444fa7c76b3bdc2992
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2017-12-24 11:03:30 -0800

    Update ec2_append_etc_hosts.sh

--- ec2_append_etc_hosts.sh
+++ ec2_append_etc_hosts.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-# Make sure both localhost and the AWS EC2 hostname have been defined
-
-echo "127.0.0.1 localhost" | sudo tee --append /etc/hosts
-echo "127.0.0.1" `ec2metadata --local-hostname` | sudo tee --append /etc/hosts
-
-## Remove --append to overwrite the file
+# Make sure both localhost and the AWS EC2 hostname have been defined in /etc/hosts
+## Remove -a (append) to overwrite the file
+echo "127.0.0.1 localhost" | sudo tee -a /etc/hosts
+echo "127.0.0.1" `ec2metadata --local-hostname` | sudo tee - /etc/hosts
