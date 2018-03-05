dec8e4f05cb858494d97fee20fd8c394a4953a34
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2018-03-05 14:40:06 -0800

    Create slack_email_address.sh

---
+++ slack_email_address.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Assumes that jq is install (via brew install jq on macOS)
+command -v jq >/dev/null 2>&1 || { echo >&2 "You will need jq in order to use this tool, installing it now"; brew install jq; }
+
+# Export can be found here: https://api.slack.com/methods/users.list/test
+# Note that the CSV download (as admin, users page) may be easier to use
+# Change this to match your export
+JSON_EXPORT=slack_users.json
+
+# Filter json file using '' and -r to only output email addresses and to remove quotes
+jq -r '.members[].profile.email' $JSON_EXPORT
