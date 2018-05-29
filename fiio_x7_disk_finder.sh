43ded73cb4c2f32b07082281067d765fdc1fd41b
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2018-05-29 11:56:57 -0700

    Update fiio_x7_disk_finder.sh

--- fiio_x7_disk_finder.sh
+++ fiio_x7_disk_finder.sh
@@ -5,4 +5,13 @@
 # Used for my Fiio X7 as the drive/mount points seem to float aroud
 # (probably as I'm not always unmounting properly)
 
+# Usage: for $i in $(fiio_disk_finder.sh); do mount -t ALLTHETHINGS; done
+
+# explanation: 
+# use -i for lsbk in order to not print out ANSI tree chars
+# grab the last three lines of the output
+# grab the first column
+# grab the odd lines
+# remove the ASCII tree chars
+
 lsblk -i | tail -3 | awk '{print $1}' | sed -n 'p;n' | tr -d \`-
