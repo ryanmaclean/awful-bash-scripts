91b8ee64b98c3da948af07930f09545be4c0d28d
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2018-09-05 16:10:44 -0700

    Create ansible_local_info.sh

---
+++ ansible_local_info.sh
@@ -0,0 +1,19 @@
+#!/usr/env bash
+cat << EOF > test.yml
+---
+- hosts: all
+  vars:
+    dest: ~/dist_info.txt
+  tasks:
+    - copy:
+        content: ''
+        dest: "{{ dest }}"
+      run_once: yes
+      delegate_to: 127.0.0.1
+    - lineinfile:
+        dest: "{{ dest }}"
+        line: 'Hostname: \t\t{{ inventory_hostname }}\nDistribution: \t\t{{ ansible_distribution }}\nDist Major Version: \t{{ ansible_distribution_major_version }}\nAnsible Dist Version: \t{{ ansible_distribution_version }}\nAnsible Dist Release: \t{{ ansible_distribution_release }}'
+      delegate_to: 127.0.0.1
+EOF
+sudo ansible-playbook test.yml -c local -i "localhost,"
+cat dist_info.txt
