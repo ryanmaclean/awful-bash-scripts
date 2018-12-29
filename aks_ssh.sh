a7893e0be48a7a5100f67d75e900d4e9970f1869
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2018-12-29 00:03:13 -0800

    Update aks_ssh.sh

--- aks_ssh.sh
+++ aks_ssh.sh
@@ -2,14 +2,14 @@
 # Add your public key to an Azure AKS node in order to... 
 # use an anti-pattern and SSH to the node!
 
+# Our resource group
+RGNAME=aks-test
+
 # Check that az cli is working
 hash az || { echo >&2 "Error: az cli not working"; exit 1; }
 
 # Check that we have a pub key
 stat ~/.ssh/id_rsa.pub || {echo >&2 "Error: pub key not found"; exit 1; }
- 
-# Our resource group
-RGNAME=aks-test
 
 # Grab 1st line, 8th column, magic node name column!
 NODE=$(az vm list -o tsv -g "${RGNAME}" | awk 'NR==1{print $8}') 
