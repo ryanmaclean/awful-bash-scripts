9cc7c30bede10410903558ddb2d7e7df57051f67
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2020-05-26 19:13:17 -0700

    Create k8s_get_token.sh

---
+++ k8s_get_token.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+# This is used to get access to the Kubernetes dashboard (1.17+), which now requires auth
+# TODO: Use shyaml, if installed, otherwise, grab the last entry
+
+# Fail on any error
+set -Eeuox pipefail
+
+# Grab token from last line of kubeconfig, second column,
+TOKEN=$(awk 'END {print $2}' $HOME/.kube/config)
+
+# Put token in the clipboard, if on macOS, else print out
+case "${unameOut}" in
+    Darwin*)
+      pbcopy $TOKEN
+      ;;
+    *)
+      echo $TOKEN
+esac
