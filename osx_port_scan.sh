f05fd4e1209e17d877a1fd10533f1f6f4ce39862
Ryan MacLean <6923433+ryanmaclean@users.noreply.github.com>
2020-12-27 20:40:36 -0800

    Fixed shebang
    Bash is no longer the default terminal in macOS, commented out the installation commands...

--- osx_port_scan.sh
+++ osx_port_scan.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-# Apple OSX Netcat Port Scan Script
+#!/usr/bin/env bash
+# Apple macOS Netcat Port Scan Script
 ###################################
 # Nothing fancy, just a loop with GNU Netcat
 
 # Check to see if Homebrew is installed, and install it if it is not
-command -v brew >/dev/null 2>&1 || { echo >&2 "You will need Homebrew to use this tool, installing now"; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+#command -v brew >/dev/null 2>&1 || { echo >&2 "You will need Homebrew to use this tool, installing now"; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
 
 # Check to see if `netcat` is installed, install it if it is not
-command -v netcat >/dev/null 2>&1 || { echo >&2 "You will also need netcat in order to use this tool, installing it now"; brew install netcat; }
+#command -v netcat >/dev/null 2>&1 || { echo >&2 "You will also need netcat in order to use this tool, installing it now"; brew install netcat; }
 
 # Prompt the user for the port which we will scan
 read -p "Which port would you like to scan? The default is [80]:" PORT
 PORT=${PORT:-80}
 
 # Prompt the user for the C-class range we will scan
-read -p "For which range? The default is [10.0.1.]:" RANGE
-RANGE=${RANGE:-10.0.1.}
+read -p "For which range? The default is [10.0.0.]:" RANGE
+RANGE=${RANGE:-10.0.0.}
 
 # Removed the start and end IP blocks and scan 255 by default
 #echo "From what starting IP? The default is 1"
